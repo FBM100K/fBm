947e3fb4938464ac9e1115b0594d2a233212a43c
FBM100K <fundsbecomemillions@gmail.com>
2025-10-28 18:19:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -576,7 +576,7 @@ with tab2:
         
         if not positions.empty:
             st.subheader("Positions ouvertes")
-            display_positions = positions[["Ticker", "Quantité", "PRU", "Devise", "Prix_actuel", "Valeur_display", "PnL_latent", "PnL_latent_%"]].copy()
+            display_positions = positions[["Ticker","Nom complet", "Quantité", "PRU", "Devise", "Prix_actuel", "Valeur_display", "PnL_latent", "PnL_latent_%"]].copy()
             display_positions.columns = ["Ticker", "Qté", "PRU", "Dev", "Prix actuel", "Valeur", "PnL €/$", "PnL %"]
             display_positions = display_positions.sort_values("PnL €/$", ascending=False)
             st.dataframe(display_positions, use_container_width=True, hide_index=True)
